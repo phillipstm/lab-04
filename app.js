e23424d8c0ac40f001b9f38fe56144657596f4dc
phillipstm <luvmargaritas@gmail.com>
2022-03-17 14:14:56 -0500

    add if else

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ let userName = prompt('What is your name?');
 
 // console.log(userName);
 //alert is another method.
-alert('Hello ' + userName);
+alert('Welcome ' + userName);
 
 
 //ask what time is it? 
 //variable assignment and declaration
-let userTime = prompt('What time is it?');
-// console.log(userTime);
+let help = prompt('How can I help you, + userName?');
+console.log('Do you need help',help);
 
 //have some logic to respond to the user.
 // if(this is true){
@@ -36,23 +36,23 @@ let userTime = prompt('What time is it?');
 // }
 
 // variable declaration creates locaiton in memory so i can store one of the messages below.
-let message;
+// let message;
 
-if(userTime <= 11){
-  message = 'Good Morning';
-} else if(userTime <= 18){
-  message = 'Good Afternoon';
-} else if (userTime <= 24){
+if('yes' || 'YES' || 'Yes'){
+  message = 'Do you need the phone number?';
+} else if('no' || 'NO' || 'No'){
+  message = 'Do you need address?';
+} else if ('maybe' || 'MAYBE' || 'Maybe'){
   message = 'Good Evening';
-} else {
-  message = 'Your input does not work for a time.';
-} 
+// } else {
+//   message = 'Your input does not work for a time.';
+// } 
 
-//this will output the message variable with the assigned value based on the if else conditional.
-// console.log('This is the if output ', message);
-
-
-//out put the users response to the page. 
+// //this will output the message variable with the assigned value based on the if else conditional.
+// // console.log('This is the if output ', message);
 
 
-document.write('Hello ' + userName +  '! '   + message);
+// //out put the users response to the page. 
+
+
+// document.write('Hello ' + userName +  '! '   + message);
