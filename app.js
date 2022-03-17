8a78e026425e778b2d4517f4ca44e795279f27ee
phillipstm <luvmargaritas@gmail.com>
2022-03-17 15:49:28 -0500

    removed comments

--- app.js
+++ app.js
@@ -1,41 +1,12 @@
 'use strict';
 console.log('I\'m hustling.');
 
-//comment
-/**
- * comment
- * enter
- * another star
- * multi line comment
- * 
- */
-
-//ask the user their name
-// var craig = 'craig';
-// console.log(craig);
-              //  prompt is a method that opens the prompt window with an input box for the user name
 let userName = prompt('What is your name?');
 
-// console.log(userName);
-//alert is another method.
-alert('Welcome ' + userName);
-
-
-//ask what time is it? 
-//variable assignment and declaration
 let help = prompt('Do you need help, ' + userName + '?');
 console.log('Do you need help',help);
 
-//have some logic to respond to the user.
-// if(this is true){
-//  then we should do this.
-// } else if(this is true){
-//   we will do this.
-// } else {
-//   lets just do this.
-// }
 
-// variable declaration creates locaiton in memory so i can store one of the messages below.
 let message;
 
 if('yes' || 'YES' || 'Yes'){
@@ -48,11 +19,6 @@ if('yes' || 'YES' || 'Yes'){
   message = 'Your input does not work for a time.';
 } 
 
-// //this will output the message variable with the assigned value based on the if else conditional.
-// // console.log('This is the if output ', message);
-
-
-// //out put the users response to the page. 
 
 
 document.write('Hello ' + userName +  '! '   + message);
