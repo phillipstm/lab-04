7bbe1f0e726d0cdda2076f16f113d5a6619c0a0e
phillipstm <luvmargaritas@gmail.com>
2022-03-18 12:18:57 -0500

    added JS finction phone user

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 'use strict';
 console.log('I\'m hustling.');
 
-let userName = prompt('What is your name?');
+function userName(){  
+  let userName = prompt('What is your name?');
 
-let help = prompt('Do you need help, ' + userName + '?');
-console.log('Do you need help',help);
-
-
-let message;
-
-if('yes' || 'YES' || 'Yes'){
-  message = 'Do you need the phone number?';
-} else if('no' || 'NO' || 'No'){
-  message = 'Do you need address?';
-} else if ('maybe' || 'MAYBE' || 'Maybe'){
-  message = 'Good Evening';
-} else {
-  message = 'Your input does not work for a time.';
-} 
+  console.log(userName);
+}
 
 
 
-document.write('Hello ' + userName +  '! '   + message);
+
+function phoneNumber(){
+  let help = prompt('Do you need the phone number, ' + userName + '?');
+  console.log('Do you need phone number',help);
+
+  if('yes' || 'YES' || 'Yes'){
+  let message = alert('867-5309');
+
+  } else if('no' || 'NO' || 'No')
+  {
+    alert('Ok, Thank You have a nice day');
+  
+  }
+}
+
+
+
